d951baf075a6e1d113dc1312e3577a2af8473629
aizifix <131889237+aizifix@users.noreply.github.com>
2024-02-06 10:15:14 +0800

    main.js

--- main.js
+++ main.js
@@ -20,24 +20,90 @@ const todoContainer = document.querySelector('.todo-container');
 
 const toasterContainer = document.querySelector('.toaster-container')
 
+
+// Light mode & Dark Mode 
+let isDarkMode = false;
+const dark = document.querySelector('#dark');
+
+// Individual Icon Toggle Colors
+function applyIconStyles() {
+    let icons = document.querySelectorAll('i');
+    icons.forEach(function (icon) {
+        if (isDarkMode) {
+            icon.style.backgroundColor = 'hsl(0, 0%, 94%)';
+            icon.style.color = 'hsl(0, 0%, 5%)';
+        } else {
+            icon.style.backgroundColor = '';
+            icon.style.color = '';
+        }
+    });
+}
+
+// For Each Input Fields Toggle
+function textStyles() {
+    let allText = document.querySelectorAll('p');
+    allText.forEach(function (text) {
+        text.style.color = isDarkMode ? 'hsl(0, 0%, 94%)' : '';
+    });
+}
+
+function inputStyles() {
+    let allInput = document.querySelectorAll('input');
+    allInput.forEach(function (inputStyle) {
+        if (isDarkMode) {
+            inputStyle.style.backgroundColor = 'hsl(0, 0%, 5%)';
+            inputStyle.style.color = 'hsl(0, 0%, 94%)';
+        } else {
+            inputStyle.style.backgroundColor = '';
+            inputStyle.style.color = '';
+        }
+    });
+}
+
+// Dark Mode Toggle Logic
+dark.addEventListener('click', () => {
+    if (isDarkMode = !isDarkMode) {
+        document.querySelector('body').style.backgroundColor = isDarkMode ? 'hsl(0, 0%, 5%)' : '';
+        document.querySelector('h1').style.color = isDarkMode ? 'hsl(0, 0%, 94%)' : '';
+        document.querySelector('button').style.backgroundColor = isDarkMode ? 'hsl(0, 0%, 94%)' : '';
+        document.querySelector('button').style.color = isDarkMode ? 'hsl(0, 0%, 5%)' : '';
+        dark.className = 'bx bx-sun';
+        applyIconStyles();
+        textStyles();
+        inputStyles();
+    } else {
+        document.querySelector('body').style.backgroundColor = isDarkMode ? 'hsl(0, 0%, 5%)' : '';
+        document.querySelector('h1').style.color = isDarkMode ? 'hsl(0, 0%, 94%)' : '';
+        document.querySelector('button').style.backgroundColor = isDarkMode ? 'hsl(0, 0%, 94%)' : '';
+        document.querySelector('button').style.color = isDarkMode ? 'hsl(0, 0%, 5%)' : '';
+        dark.className = 'bx bx-moon';
+        applyIconStyles();
+        textStyles();
+        inputStyles();
+    }
+});
+
+
+
 // initial value for the task counter 
 let countNum = 0;
-
 
 // Incrementing values for each tasks created
 addTask.addEventListener('click', () => {
     if (todoInput.value.trim() != '') {
+        toasterSuccess();
         inputCard(todoInput.value);
         todoInput.value = '';
         countNum += 1;
+        counter.style.color = 'green'
         counter.innerHTML = countNum;
     } else {
-        toaster();
+        toasterEmpty();
     }
 });
 
 // This will only appear if tasks added are blank
-function toaster() {
+function toasterEmpty() {
     const toaster = document.createElement('div');
     toaster.className = 'toaster';
 
@@ -48,13 +114,35 @@ function toaster() {
     toasterContainer.appendChild(toaster);
 
     // This is the time duration of the toaster hover
+
     setTimeout(() => {
         toaster.remove();
-    }, 3000);
+    }, 1200);
+
 }
+
+// Toaster for task created
+function toasterSuccess() {
+    const addToaster = document.createElement('div');
+    addToaster.className = 'addToaster';
+
+    const addtoasterMsg = document.createElement('p');
+    addtoasterMsg.textContent = 'Success! Task added';
+
+    addToaster.appendChild(addtoasterMsg);
+    toasterContainer.appendChild(addToaster);
+
+    setTimeout(() => {
+        addToaster.remove();
+    }, 1200);
+
+}
+
 
 // This is the card creation during on click process
 function inputCard(inputTask) {
+
+
     let card = document.createElement('div');
     card.className = 'card';
 
@@ -102,6 +190,9 @@ function inputCard(inputTask) {
 
     taskContainer.appendChild(card);
 
+    applyIconStyles();
+    inputStyles();
+
     // This is use to edit a card
     editInput.addEventListener('click', () => {
         inputCard.removeAttribute('readonly');
@@ -111,7 +202,7 @@ function inputCard(inputTask) {
         inputCard.style.cursor = 'auto';
 
         // Save new text 
-        saveInput.addEventListener('click', ()=> {
+        saveInput.addEventListener('click', () => {
             inputCard.readOnly = true;
             inputCard.focus();
             inputCard.style.color = '';
@@ -125,11 +216,9 @@ function inputCard(inputTask) {
         card.remove();
         countNum -= 1;
         counter.innerHTML = countNum;
+        if (countNum == 0) {
+            counter.style.color = 'red';
+        }
     });
 
 }
-
-
-
-
-
